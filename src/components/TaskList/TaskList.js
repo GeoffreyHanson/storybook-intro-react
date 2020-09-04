6fe1ab31faf029a7e24892962085032345bfcdf3
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-09-04 13:12:23 -0500

    Finished intro part 4

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Task from '../Task/Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../../lib/redux';
 
 // This list contains all logic for separate states.
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   // Events for storybook?
   const events = {
     onPinTask,
@@ -60,7 +62,7 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
@@ -71,6 +73,16 @@ TaskList.propTypes = {
   onArchiveTask: PropTypes.func,
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINED'),
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
